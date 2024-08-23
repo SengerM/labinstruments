7d842f70e7884d301dac8490e6baf72352e090a6
Matias Senger <m.senger@hotmail.com>
2024-08-23 14:44:26 +0100

    Add readme and setup

---
+++ readme.md
@@ -0,0 +1,9 @@
+# labinstruments
+
+A collection of Python utilities to control laboratory instruments.
+
+## Installation
+
+```
+pip install git+https://github.com/SengerM/labinstruments
+```
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name = "labinstruments",
+	version = "0",
+	author = "Matias Senger",
+	author_email = "m.senger@hotmail.com",
+	description = "A collection of things to ease the communication with laboratory insturmentation",
+	url = "https://github.com/SengerM/labinstruments",
+	packages = setuptools.find_packages(),
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
