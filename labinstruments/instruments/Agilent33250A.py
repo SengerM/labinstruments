e71638fcc099c43201f95589e4a1758dca15b1fb
Matias Senger <m.senger@hotmail.com>
2024-08-26 15:47:00 +0100

    Removed all buggy methods to configure individual parameters and implemented the simple apply method as suggested in the user manual

--- labinstruments/instruments/Agilent33250A.py
+++ labinstruments/instruments/Agilent33250A.py
@@ -5,23 +5,9 @@ class Agilent33250A(SCPISerialInstrument):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, instrument_manufacturer='Agilent', instrument_model='33250A', **kwargs)
 
-	def set_shape(self, shape:str):
-		self.write(f'FUNC {shape.upper()}')
-		if shape.lower() == 'user':
-			self.write('FUNC:USER volatile') # From all the arbitrary waveforms, select the one in the volatile memory.
-
-	def set_frequency(self, hertz:float):
-		self.write(f'FREQ {hertz:e}')
-
-	def set_amplitude(self, volts_pp:float):
+	def apply(self, function:str, frequency:float, volt_pp:float, volt_offset:float):
 		self.write('VOLT:UNIT VPP') # Make sure it will be volts peak to peak.
-		self.write(f'VOLT {volts_pp:e}')
-
-	def set_offset(self, volts:float):
-		self.write(f'VOLT:OFFS {volts:e}')
-
-	def set_pulse_width(self, seconds:float):
-		self.write(f'PULS:WIDT {seconds:e}')
+		self.write(f'APPLY:{function} {frequency:e}, {volt_pp:e}, {volt_offset:e}')
 
 	def set_output(self, status:str):
 		if status.lower() not in {'on','off'}:
@@ -45,43 +31,26 @@ class Agilent33250A(SCPISerialInstrument):
 		self.write('*TRG')
 		self.write('*WAI')
 
-	def wait_for_trigger(self, timeout=None, should_stop=lambda: False):
-		""" Wait until the triggering has finished or timeout is reached.
-		:param timeout: The maximum time the waiting is allowed to take. If
-						timeout is exceeded, a TimeoutError is raised. If
-						timeout is set to zero, no timeout will be used.
-		:param should_stop: Optional function (returning a bool) to allow the
-							waiting to be stopped before its end.
-		"""
-		self.write("*OPC?")
-		t0 = time()
-		while True:
-			try:
-				ready = bool(self.read())
-			except Exception:
-				ready = False
-			if ready:
-				return
-			if timeout is not None and time() - t0 > timeout:
-				raise TimeoutError("Timeout expired while waiting for the Agilent 33220A to finish the triggering.")
-
 	def output_triggered(self, status:str):
 		if status.lower() not in {'on','off'}:
 			raise ValueError('`status` must be either "on" or "off". ')
 		status = 1 if status.lower()=='on' else 0
 		self.write(f'OUTP:TRIG {status}')
 
-	def configure_arbitrary_waveform(self, samples_in_volt:list[float]):
+	def configure_arbitrary_waveform(self, samples_in_volt:list[float], frequency:float):
 		# Compute normalized samples between -1 and 1:
 		maximum_absolute_voltage = [abs(s) for s in samples_in_volt]
 		maximum_absolute_voltage = max(maximum_absolute_voltage)
 		samples = samples_in_volt
 		samples = [s/maximum_absolute_voltage for s in samples]
 		self.load_arbitrary_waveform_samples(samples)
-		# Now configure offset and amplitude to obtain the desired voltage levels:
-		self.set_offset(volts=0)
-		self.set_amplitude(volts_pp=max(samples_in_volt)-min(samples_in_volt))
-		self.set_shape('user') # Select arbitrary waveform source.
+		self.write('FUNC:USER volatile') # From all the arbitrary waveforms, select the one in the volatile memory.
+		self.apply(
+			function = 'user',
+			frequency = frequency,
+			volt_pp = (max(samples_in_volt)-min(samples_in_volt))/2,
+			volt_offset = 0,
+		)
 
 	def load_arbitrary_waveform_samples(self, samples:list[float]):
 		self.write_without_checking_errors('DATA VOLATILE, ' + ', '.join([str(_) for _ in samples]))
@@ -102,12 +71,11 @@ def example():
 
 	A.set_output('off')
 
-	A.configure_arbitrary_waveform([0,-1,0,-2,0,0])
+	A.configure_arbitrary_waveform([0,3,0,-2,0,0], frequency=1e6)
 	A.set_burst('on')
 	A.set_burst_mode('triggered')
-	A.set_burst_n_cycles(2)
+	A.set_burst_n_cycles(2222)
 	A.write('TRIG:SOURCE BUS') # Select trigger source from software.
-	A.set_frequency(1e3)
 	A.set_output('on')
 	A.force_trigger()
 
@@ -115,11 +83,11 @@ if __name__ == '__main__':
 	import sys
 	import logging
 
-	logging.basicConfig(
-		stream = sys.stderr,
-		level = logging.DEBUG,
-		format = '%(asctime)s|%(levelname)s|%(message)s',
-		datefmt = '%H:%M:%S',
-	)
+	# ~ logging.basicConfig(
+		# ~ stream = sys.stderr,
+		# ~ level = logging.DEBUG,
+		# ~ format = '%(asctime)s|%(levelname)s|%(message)s',
+		# ~ datefmt = '%H:%M:%S',
+	# ~ )
 
 	example()
