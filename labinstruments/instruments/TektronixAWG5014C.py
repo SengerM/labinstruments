663aa4d2c446a92081ce199e6a6d404385b55e21
Matias Senger <m.senger@hotmail.com>
2025-01-23 13:11:29 +0100

    Implemented some methods

--- labinstruments/instruments/TektronixAWG5014C.py
+++ labinstruments/instruments/TektronixAWG5014C.py
@@ -1,6 +1,12 @@
 from labinstruments.Instrument import SCPIInstrument
 import socket
 import logging
+import numpy
+import array as arr
+
+# Reference example: https://github.com/FermilabQuantumNetwork/CQNET-AWG
+# Better example: http://microsoft.github.io/Qcodes/_modules/qcodes/instrument_drivers/tektronix/AWG5014.html#TektronixAWG5014
+
 logger = logging.getLogger(__name__)
 
 class TektronixAWG5014C(SCPIInstrument):
@@ -30,24 +36,81 @@ class TektronixAWG5014C(SCPIInstrument):
 		logger.debug(f'Received: {received}')
 		return received
 
+	def set_output(self, n_channel:int, status:str):
+		"""Set the output of the channel `n_channel` to `'on'` or `'off'`."""
+		self.write(f'OUTPUT{n_channel} {status}')
+
+	def delete_waveform(self, name:str):
+		self.write(f'WLISt:WAVeform:DELete "{name}"') # Delete any waveform that is already using the same name.
+
+	def load_arbitrary_waveform_samples(self,
+		name:str, # The name of the waveform.
+		samples:list, # A list of `float` or `int` with the samples of the waveform.
+		markers_1:list, # A list with the markers 1.
+		markers_2:list, # A list with the markers 2.
+		override:bool=False # If `True`, any waveform that is already in the memory of the AWG with the same name will be deleted before loading this new waveform.
+	):
+		if not len(markers_1) == len(markers_2) == len(samples):
+			raise ValueError(f'`markers_1`, `markers_2` and `samples` must have the same length. ')
+
+		if min(samples) < -1 or max(samples) > 1:
+			raise TypeError('`samples` can only contain values between -1 and 1 (inclusive). ')
+		if not set(markers_1) <= {0,1} or not set(markers_2) <= {0,1}:
+			raise ValueError(f'`markers_1` and `markers_2` can only contain 0s and 1s, but they have {set(markers_1)} and {set(markers_2)}, respectively. ')
+
+		if override:
+			try:
+				self.delete_waveform(name)
+			except RuntimeError as e:
+				if '4000, "Waveform editing error; E11110' in repr(e):
+					# This happens when the waveform does not exist in the AWG memory.
+					pass
+		try:
+			self.write(f'WLISt:WAVeform:NEW "{name}", {len(samples)}, INT') # Create a new empty waveform where the samples are going to be loaded.
+		except RuntimeError as e:
+			if '4000,"Waveform editing error; E11110' in repr(e):
+				raise RuntimeError(f'A waveform with the name {repr(name)} already exists in the AWG. ')
+
+		# The following block of code was copy-pasted from [the source code of Qcodes](http://microsoft.github.io/Qcodes/_modules/qcodes/instrument_drivers/tektronix/AWG5014.html#TektronixAWG5014.send_waveform_to_list). I'm sorry, but it was taking too much time.
+		number = (2**13 - 1) + (2**13 - 1) * numpy.array(samples) + 2**14 * numpy.array(markers_1) + 2**15 * numpy.array(markers_2)
+		number = number.astype("int")
+		ws_array = arr.array("H", number)
+		ws = ws_array.tobytes()
+		s1_str = f'WLISt:WAVeform:DATA "{name}",'
+		s1 = s1_str.encode("UTF-8")
+		s3 = ws
+		s2_str = "#" + str(len(str(len(s3)))) + str(len(s3))
+		s2 = s2_str.encode("UTF-8")
+		mes = s1 + s2 + s3 + b'\n'
+		logger.debug(f'Sending command: {mes}')
+		self.socket.sendall(mes)
+
+		self.check_whether_error()
+
 def example():
 	awg = TektronixAWG5014C(
 		ip_address = '192.168.0.69',
 		port = 1111,
 	)
-
+	awg.clear_errors_buffer()
 	print(awg.idn)
-	print(awg.query('AWGCONTROL:DC1:STATE?'))
+	awg.load_arbitrary_waveform_samples(
+		name = 'deleteme',
+		samples = [0,1,0,1,-1,.25,0,1],
+		markers_1 = [1,0,0,0,0,0,0,0],
+		markers_2 = [0,0,0,0,1,0,0,1],
+		override = True,
+	)
 
 if __name__ == '__main__':
 	import sys
 	import logging
 
-	logging.basicConfig(
-		stream = sys.stderr,
-		level = logging.DEBUG,
-		format = '%(asctime)s|%(levelname)s|%(message)s',
-		datefmt = '%H:%M:%S',
-	)
+	# ~ logging.basicConfig(
+		# ~ stream = sys.stderr,
+		# ~ level = logging.DEBUG,
+		# ~ format = '%(asctime)s|%(levelname)s|%(message)s',
+		# ~ datefmt = '%H:%M:%S',
+	# ~ )
 
 	example()
